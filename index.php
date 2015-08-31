d4c1a06bc3e55fddb9d94c7dafcd959ff6cd07af
mycmms5 <werner.huysmans@skynet.be>
2015-08-31 13:47:25 +0200

    Startpoint of MYCMMS
    ...

---
+++ index.php
@@ -0,0 +1,50 @@
+<?PHP
+/** 
+* Startpage for MYCMMS with full Smarty support
+* 
+* @author  Werner Huysmans 
+* @access  public
+* @package framework
+* @subpackage framework
+* @filesource
+* @tpl fw/index.tpl
+* CVS
+* $Id: index.php,v 5.0 2015/07/26 12:35:00 werner Exp $
+* $Source: /var/www/cvs/mycmms40/mycmms40/index.php,v $
+* $Log: index.php,v $
+* Revision 5.0  
+* -added support for database configuration; we no longer need to change code to change behavior; system and tabdefault are in table sys_system
+*/ 
+$nosecurity_check=true;
+$rootfile=true;
+require("./includes/config_mycmms.inc.php");    # direct path
+require("./libraries/setup.php");
+$DB=DBC::get();
+$result=$DB->query("SELECT * FROM sys_system WHERE id=0");
+$row=$result->fetch(PDO::FETCH_ASSOC);
+if(!empty($_GET['nav'])) {
+    $_SESSION['nav']=$_GET['nav'];    
+    $_SESSION['system']=$row['system'];
+} else {
+    $_SESSION['nav']=$row['tabdefault'];
+    $_SESSION['system']=$row['system'];
+}
+/**
+* In order to construct the main page we will retrieve the Tabs and their default actions from the table sys_mainwindow
+* - tab= name of the TAB (will be translated by GetText)
+* - tabdefault = default list (WO_APPROVED for example will get the data as defined in sys_queries)
+*/
+$result=$DB->query("SELECT tab,tabdefault FROM sys_mainwindow WHERE system='{$_SESSION['system']}' ORDER BY taborder");
+foreach ($result->fetchAll(PDO::FETCH_ASSOC) AS $tab_action) {
+    $action[$tab_action['tab']]=$tab_action['tabdefault'];   
+}
+$tpl=new smarty_mycmms();
+$tpl->debugging=false;
+$tpl->caching=false;
+$tpl->assign("page_title",CMMS_TITLE);
+$tpl->assign('version',"$Id: index.php,v 1.2 2013/12/25 09:04:47 werner Exp $");
+$tpl->assign("nav",$_SESSION['nav']);
+$tpl->assign("query",$action[$_SESSION['nav']]);
+$tpl->assign("title","title.php");
+$tpl->display_error("fw/index.tpl");
+?>
